dd7ec98ac6b9e4b046a421262ec83b25a621124b
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-04-01 23:36:11 +0400

    Added props

--- src/components/PopupModal.vue
+++ src/components/PopupModal.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="absolute w-full bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
-        <div class="p-4 bg-secondary-color self-start mt-32 max-w-screen-md">
+    <div 
+    v-show="modalActive"
+    class="absolute w-full bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
+        <div 
+        v-if="modalActive"
+        class="p-4 bg-secondary-color self-start mt-32 max-w-screen-md">
             <slot />
             <button class="text-white rounded-full mt-8 bg-primary-color py-2 px-6">Close</button>
         </div>
@@ -8,6 +12,11 @@
 </template>
 
 <script setup>
-
+defineProps({
+    modalActive: {
+        type: Boolean,
+        default: false,
+    }
+})
 </script>
 
