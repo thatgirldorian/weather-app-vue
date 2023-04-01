d4a78b96190a356975d8f474e931c051f0d10e60
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-04-01 13:09:18 +0400

    Added nav component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
+    <NavigationBar />
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import {RouterView} from "vue-router"
+import NavigationBar from "./components/NavigationBar.vue"
 </script>
 
 <style lang="css" scoped>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>NAVbR</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
