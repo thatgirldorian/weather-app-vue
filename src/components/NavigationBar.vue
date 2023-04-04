657cc580adc09462d165aa7b738bdbcd6c7c6f73
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-04-04 23:02:03 +0400

    Changed app name

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,14 +2,15 @@
     <header class="sticky top-0  shadow-lg">
         <nav class="container flex flex-col sm:flex-row items-center gap-4 text-black py-6">
            <RouterLink :to="{name: 'home'}">
-            <div class="flex items-center gap-3 flex-1">
+            <div class="flex items-center gap-3">
                 <i class="fa-solid fa-sun"></i>
-                <p class="text-xl">weathernow</p>
+                <p class="text-xl">SkyCast</p>
             </div>
            </RouterLink>
-           <div class="flex gap-3">
+           <div class="flex gap-3 flex-1 justify-end">
             <i class="fa-solid fa-circle-info text-xl hover:text-secondary-color duration-150 cursor-pointer"></i>
-           </div>
+            <i class="fa-solid fa-plus text-xl hover:text-secondary-color duration-150 cursor-pointer"></i>
+        </div>
         </nav>
     </header>
 </template>
