30e9eded7ea88cf5215f760ad5b4d53e465b8ce4
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-04-04 23:30:35 +0400

    Added modal content

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="sticky top-0  shadow-lg">
         <nav class="container flex flex-col sm:flex-row items-center gap-4 text-black py-6">
-           <RouterLink :to="{name: 'home'}">
+           <RouterLink :to="{ name: 'home' }">
             <div class="flex items-center gap-3">
                 <i class="fa-solid fa-sun"></i>
                 <p class="text-xl">SkyCast</p>
@@ -13,7 +13,30 @@
         </div>
 
         <PopupModal>
-            <h1>HIII</h1>
+            <div class="text-black">
+          <h1 class="text-2xl mb-1">Overview</h1>
+          <p class="mb-4">
+           SkyCast is an application that enables you to monitor the current and future weather in cities of your choice.
+          </p>
+          <h2 class="text-2xl">How it works</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>
+                To track the weather in a city, type its name into the search bar and select it from the results.
+            </li>
+            <li>
+                This will take you to the current weather for your selected city.
+            </li>
+            <li>
+                To save a city for later viewing on the home page, click on the "+" icon in the top right and track the city.
+            </li>
+          </ol>
+
+          <h2 class="text-2xl">Delete a city</h2>
+          <p>
+            If you no longer want to track a city, navigate to the home page, select the city, and click on the delete option at the bottom of the page.
+
+          </p>
+        </div>
         </PopupModal>
         </nav>
     </header>
@@ -21,6 +44,6 @@
 
 <script setup>
 import { RouterLink } from 'vue-router';
-import { PopupModal } from './PopupModal.vue'
+import  PopupModal from './PopupModal.vue'
 </script>
 
--- src/components/PopupModal.vue
+++ src/components/PopupModal.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="absolute w-full bg-red bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
-        <div class="p-4 bg-black mt-32 max-w-screen-md">
+    <div class="absolute w-full bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
+        <div class="p-4 bg-secondary-color self-start mt-32 max-w-screen-md">
             <slot />
-            <button class="text-black mt-8 bg-primary-color py-2 px-6">Close</button>
+            <button class="text-white rounded-full mt-8 bg-primary-color py-2 px-6">Close</button>
         </div>
     </div>
 </template>
