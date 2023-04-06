05fe7bd877e8132c4bbd945a9ca5ebb08504b15c
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-04-07 00:40:57 +0400

    Added a search bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <script setup></script>
 
 <template>
-    <main></main>
+    <main class="container text-black">
+        <div class="pt-4 mb-8 relative">
+            <input type="text" placeholder="Look up a city or state :)"
+            class="py-2 px-1 w-full bg-transparent border-b focus:border-secondary-color focus:outline-none focus:shadow-[0px_1px_0_0_#a8dadc]"
+            >
+        </div>
+    </main>
 </template>
