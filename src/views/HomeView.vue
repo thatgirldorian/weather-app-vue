0cdd6b4cf7e1c95c9bbe4a0332102eed6acb55ff
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-04-08 00:47:36 +0400

    Added a search function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,17 @@
 <template>
     <main class="container text-black">
         <div class="pt-4 mb-8 relative">
-            <input type="text" placeholder="Look up a city or state :)"
+            <input type="text" 
+            v-model="searchQuery"
+            placeholder="Look up a city or state :)"
             class="py-2 px-1 w-full bg-transparent border-b focus:border-secondary-color focus:outline-none focus:shadow-[0px_1px_0_0_#a8dadc]"
             >
         </div>
     </main>
 </template>
+
+<script>
+import { ref } from "vue"
+
+const searchQuery = ref("")
+</script>
