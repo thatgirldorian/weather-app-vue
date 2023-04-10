45f7a757523d055418ac31b86c39890f5f5b4159
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-04-10 23:28:43 +0400

    Added transition elements to the popup

--- src/components/PopupModal.vue
+++ src/components/PopupModal.vue
@@ -1,10 +1,10 @@
 <template>
-    <Transition>
+    <Transition name="modal-outer">
         <div 
     v-show="modalActive"
     class="absolute w-full bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
-       <Transition>
-        <div 
+       <Transition name="modal-inner">
+        <div  
         v-if="modalActive"
         class="p-4 bg-secondary-color self-start mt-32 max-w-screen-md">
             <slot />
@@ -26,4 +26,12 @@ defineProps({
     }
 })
 </script>
+
+<style scoped>
+.modal-outer-enter-active, .modal-outer-leave-active {
+    transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+
+</style>
 
